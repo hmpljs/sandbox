1f86b8b448b73bb44ca85fb49da16183bffc39fd
antonmak1 <antonmakdev@gmail.com>
2024-07-13 01:43:12 +0300

    commit

--- routes/get.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const expressRouter = express.Router();
-const path = require("path");
-
-expressRouter.use("/test", (req, res) => {
-  res.sendFile(path.join(__dirname, "../serverHTML/GET/test.html"));
-});
-
-module.exports = expressRouter;
